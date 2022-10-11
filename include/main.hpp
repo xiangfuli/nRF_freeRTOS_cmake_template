347484b1efeec9c5c8f08c38b620484d4d3e2725
xiangfuli <xiangfuli96@gmail.com>
2022-10-10 21:18:50 -0400

    Add blinking LED example

--- include/main.hpp
+++ include/main.hpp
@@ -1,6 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include "FreeRTOS.h"
+#include "task.h"
+#include "nrf_gpio.h"
+#include "nrf.h"
+
 int main(void);
 
 #endif /* MAIN_H */
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,28 @@
 #include "main.hpp"
 
+#define LED_PIN   41
+
+void task_led_blinking(void *pvParameter) {
+  nrf_gpio_cfg_output(LED_PIN);
+
+  while(1) {
+    NRFX_DELAY_US(1000 * 1000);
+    nrf_gpio_pin_toggle(LED_PIN);
+  }
+}
+
+
 int main() {
-  
+
+  xTaskCreate(
+    task_led_blinking,
+    "task_led_blinking",
+    configMINIMAL_STACK_SIZE,
+    NULL,
+    configMAX_PRIORITIES, 
+    NULL
+  );
+
+  vTaskStartScheduler();
+  while(1) {}
 }
